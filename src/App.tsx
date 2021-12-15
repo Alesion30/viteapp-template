7f9729583c83c7e33722627b34a1c102cffae99e
Yuta Ohira <alesion90@gmail.com>
2021-12-15 13:24:49 +0900

    :sparkles: tailwindcssを導入

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,35 @@
 import { useState } from 'react'
 
-import logo from './logo.svg'
-import './App.css'
+import logo from '~/logo.svg'
 
-function App() {
+const App: React.VFC = () => {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type='button' onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
+    <div className='text-center'>
+      <header className='bg-slate-700 min-h-screen flex flex-col align-center justify-center text-3xl text-white'>
+        <img src={logo} className='h-72' alt='logo' />
+        <p className='text-4xl'>Hello Vite + React!</p>
+        <p className='my-5'>
+          count is:
+          <button
+            type='button'
+            className='bg-gray-50 hover:bg-gray-100 text-black p-2 mx-2'
+            onClick={() => setCount((count) => count + 1)}
+          >
+            {count}
           </button>
         </p>
-        <p>
+        <p className='my-2'>
           Edit <code>App.tsx</code> and save to test HMR updates.
         </p>
-        <p>
-          <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
+        <p className='my-2'>
+          <a className='text-cyan-300' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
             Learn React
           </a>
           {' | '}
           <a
-            className='App-link'
+            className='text-cyan-300'
             href='https://vitejs.dev/guide/features.html'
             target='_blank'
             rel='noopener noreferrer'
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import './index.css'
-import App from './App'
+import '~/index.css'
+import 'tailwindcss/tailwind.css'
+import App from '~/App'
 
 ReactDOM.render(
   <React.StrictMode>
