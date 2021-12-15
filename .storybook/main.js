381d61164693c789071a76993b7d6020ad784545
Yuta Ohira <alesion90@gmail.com>
2021-12-15 15:59:49 +0900

    :ambulance: storybook上においてaliasとtailwindに対応

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,26 @@
+const path = require('path')
+
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    {
+      name: '@storybook/addon-postcss',
+      options: {
+        postcssLoaderOptions: {
+          implementation: require('postcss'),
+        },
+      },
+    },
+  ],
+  webpackFinal: async (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '~': path.resolve(__dirname, '../src'),
+    }
+    return config
+  },
   framework: '@storybook/react',
   core: {
     builder: 'webpack5',
